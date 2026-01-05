56f284d8af9c13b1076f956d90017269afdb1825
Anibal Cañete <anibal_gustavo2010@hotmail.com>
2026-01-04 21:48:28 -0300

    Creación del Archivo Scripts.js

---
+++ assets/js/Scripts.js
@@ -0,0 +1,16 @@
+
+// HTML Elements
+const displayValuePrevious = document.getElementById("value-previous");
+const displayValueCurrent = document.getElementById("value-current");
+const buttonsNumbers = document.querySelectorAll(".number");
+const buttonsOperators = document.querySelectorAll(".operator");
+
+const display = new Display(displayValuePrevious, displayValueCurrent);
+
+buttonsNumbers.forEach(button => {
+    button.addEventListener("click", () => display.addNumber(button.innerHTML));
+});
+
+buttonsOperators.forEach(button => {
+    button.addEventListener("click", () => display.compute(button.value));
+});
